8b5b3c3d9b15176be8149117278173a031cab4d4
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2014-10-11 13:42:36 -0500

    Add missing files due to incorrect .hgignore file

---
+++ test/NUnit.ApplicationDomain.Tests/AbstractClassesWork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  public abstract class TestsBase
+  {
+    [Test, RunInApplicationDomainAttribute]
+    public void SomeTest()
+    {
+    }
+
+    protected abstract void SomeChildSetup();
+  }
+
+  [TestFixture]
+  public class ActualTestClass : TestsBase
+  {
+    [Test, RunInApplicationDomainAttribute]
+    [Description("Each test is in different appdomain, even works in abstract classes")]
+    public void Test1()
+    {
+      Assert.AreEqual(13, StaticInformation.Count);
+      // if we weren't in a separate app domain, the next test would fail:
+      StaticInformation.Count++;
+
+      Console.WriteLine("In Application domain");
+    }
+
+    [Test, RunInApplicationDomainAttribute]
+    [Description("Each test is in different appdomain, even works in abstract classes")]
+    public void Test2()
+    {
+      Assert.AreEqual(13, StaticInformation.Count);
+      // if we weren't in a separate app domain, the next test would fail:
+      StaticInformation.Count++;
+
+      Console.WriteLine("In Application domain");
+    }
+
+    protected override void SomeChildSetup()
+    {
+    }
+  }
+}
---
+++ test/NUnit.ApplicationDomain.Tests/AssemblyConfigTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  class AssemblyConfigTests
+  {
+  }
+}
