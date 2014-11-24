95e4bee141462ccee6b3c671d484e18f85e25515
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2014-11-23 20:26:30 -0600

    Update NuGet package/version

--- src/NUnit.ApplicationDomain/Properties/AssemblyInfo.cs
+++ src/NUnit.ApplicationDomain/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -7,11 +6,11 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("NUnit.ApplicationDomain")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Run NUnit tests in a separate application domain")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mackenzie Zastrow")]
 [assembly: AssemblyProduct("NUnit.ApplicationDomain")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
