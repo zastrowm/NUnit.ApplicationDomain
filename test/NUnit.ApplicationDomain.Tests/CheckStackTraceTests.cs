c6bb8cdf6edb7d606bfeaa390c6dc962f2946c0d
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2015-04-28 19:05:35 -0500

    Add developer test to check exception output

---
+++ test/NUnit.ApplicationDomain.Tests/CheckStackTraceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  [Explicit, RunInApplicationDomain]
+  public class CheckStackTraceTests
+  {
+    [Test]
+    [Description("Allows a developer to view the exception message that occurs when a test fails in the app domain")]
+    public void CheckExceptionMessage()
+    {
+      Console.WriteLine("In the app domain");
+      SomeMethod();
+    }
+
+    private static void SomeMethod()
+    {
+      AnotherMethod();
+    }
+
+    private static void AnotherMethod()
+    {
+      Assert.AreEqual("This", "Should Fail");
+    }
+  }
+}
