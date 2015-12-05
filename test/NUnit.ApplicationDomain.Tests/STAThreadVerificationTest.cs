bdd286edd8a237070b1456804e6cb2e079fdbc7c
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2015-12-05 14:39:58 -0600

    Add test to verify that STA attributes affect AppDomain
    addresses #11

---
+++ test/NUnit.ApplicationDomain.Tests/STAThreadVerificationTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  public class STAThreadVerificationTest
+  {
+    [Test, Apartment(ApartmentState.STA)]
+    [RunInApplicationDomain]
+    public void VerifySTA()
+    {
+      Assert.That(Thread.CurrentThread.GetApartmentState(), Is.EqualTo(ApartmentState.STA));
+    }
+  }
+}
