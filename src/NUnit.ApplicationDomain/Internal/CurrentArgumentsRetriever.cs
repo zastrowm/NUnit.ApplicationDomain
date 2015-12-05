1ea95512aae837e7ccd16af24eeed328f3111fbd
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2015-12-05 13:19:53 -0600

    Simplify accessing the current test (switch to using the exposed test property instead of using reflection)

--- src/NUnit.ApplicationDomain/Internal/CurrentArgumentsRetriever.cs
+++ src/NUnit.ApplicationDomain/Internal/CurrentArgumentsRetriever.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using NUnit.Framework.Internal;
 
 namespace NUnit.ApplicationDomain.Internal
 {
@@ -10,14 +11,6 @@ namespace NUnit.ApplicationDomain.Internal
   /// </summary>
   internal static class CurrentArgumentsRetriever
   {
-    private static readonly PropertyInfo TestExecutionContextType;
-
-    /// <summary> Static constructor. </summary>
-    static CurrentArgumentsRetriever()
-    {
-      TestExecutionContextType = FindCurrentContextProperty();
-    }
-
     /// <summary>
     ///  Attempts to get the arguments for the current executing test by using reflection to get at  
     ///  <code>NUnit.Core.TestExecutionContext.CurrentContext.arguments</code>
@@ -25,47 +18,17 @@ namespace NUnit.ApplicationDomain.Internal
     /// <returns> The current arguments for the test, or null of none are available. </returns>
     public static object[] GetCurrentTestArguments()
     {
-      // we never found it, so bail out
-      if (TestExecutionContextType == null)
-        return null;
-
-      dynamic currentContext = TestExecutionContextType.GetValue(null, null);
-
       // we don't cache the rest of the getters, because we're not sure if the properties actually
       // change or not throughout the lifecycle, so for safety we we-reflect every time. 
-      var currentTest = currentContext.CurrentTest;
+      var currentTest = TestExecutionContext.CurrentContext.CurrentTest;
+
       Type currentTestType = currentTest.GetType();
 
       var argumentsField = currentTestType.GetProperty(
         "Arguments",
         BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetProperty);
 
-      return argumentsField.GetValue(currentTest, null);
-    }
-
-    /// <summary>
-    ///  Get the property that represents the getter for
-    ///  <code>NUnit.Core.TestExecutionContext.CurrentContext</code>. This can be safely cached
-    ///  because it's a static type and a static property.
-    /// </summary>
-    private static PropertyInfo FindCurrentContextProperty()
-    {
-      var types = from assembly in AppDomain.CurrentDomain.GetAssemblies()
-                  where assembly.FullName.Contains("nunit.framework")
-                  from type in assembly.GetTypes()
-                  where type.FullName == "NUnit.Framework.Internal.TestExecutionContext"
-                  select type;
-
-      var textExecutionContextType = types.FirstOrDefault();
-      // not sure what would cause this but better safe than sorry
-      if (textExecutionContextType == null)
-        return null;
-
-
-      PropertyInfo getter = textExecutionContextType.GetProperty("CurrentContext",
-                                                                 BindingFlags.Public | BindingFlags.Static);
-
-      return getter;
+      return (object[])argumentsField.GetValue(currentTest, null);
     }
   }
 }
