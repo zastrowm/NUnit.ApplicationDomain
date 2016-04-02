c0c9f6137e9b26849570dc4d8a70ea73a4b3b67f
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-04-02 17:05:48 -0500

    Add UT to verify that the app.config gets loaded
    Resolves #1

---
+++ test/NUnit.ApplicationDomain.Tests/AppConfigTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  internal class AppConfigTests
+  {
+    [Test, RunInApplicationDomain]
+    public void VerifyAppConfig_IsLoaded()
+    {
+      string configValue = ConfigurationManager.AppSettings["verify"];
+
+      Assert.That(configValue, Is.EqualTo("that-it-loaded"));
+    }
+  }
+}
