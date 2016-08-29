c19bdb726ab6a11a313b93b50a491b3014585e18
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-08-28 21:56:20 -0500

    Update nuget package with latest changes
    Should be nuget package 8.0.0 now

--- src/NUnit.ApplicationDomain/Properties/AssemblyInfo.cs
+++ src/NUnit.ApplicationDomain/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
