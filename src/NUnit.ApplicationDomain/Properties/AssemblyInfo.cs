c56a59cd861d9b0804beab1269733a7fc6dbf82b
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-10-03 21:13:51 -0500

    Update nuspec w/latest version info
    + use version from assembly for nuspec version

--- src/NUnit.ApplicationDomain/Properties/AssemblyInfo.cs
+++ src/NUnit.ApplicationDomain/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("8.0.0.0")]
-[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyVersion("9.0.0.0")]
+[assembly: AssemblyFileVersion("9.0.0.0")]
