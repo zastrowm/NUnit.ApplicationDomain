af6cfe0745802b14f25a60c119a9c52242ef5948
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-10-15 14:28:23 -0500

    Add tests that make sure that arguments that are MarshalByRefObject or [Serializable] work

---
+++ test/NUnit.ApplicationDomain.Tests/ArgumentsCanBeSerializableOrRemoteableTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  [RunInApplicationDomain]
+  public class ArgumentsCanBeSerializableOrRemoteableTests
+  {
+    private static readonly RemotableType[] RemotableTestData
+      =
+      {
+        new RemotableType(1),
+        new RemotableType(2),
+        new RemotableType(3),
+      };
+
+    [Test]
+    [TestCaseSource(nameof(RemotableTestData))]
+    public void ArgumentsThatAreMarhsallByRefWork(RemotableType testData)
+    {
+      Assert.That(testData.Data, Is.Not.EqualTo(0));
+    }
+
+    public class RemotableType : MarshalByRefObject
+    {
+      public RemotableType(int data)
+      {
+        Data = data;
+      }
+
+      public int Data { get; }
+
+      /// <inheritdoc />
+      public override string ToString()
+        => Data.ToString();
+    }
+
+    private static readonly SerializableType[] SerialableTestData
+      =
+      {
+        new SerializableType(1),
+        new SerializableType(2),
+        new SerializableType(3),
+      };
+
+    [Test]
+    [TestCaseSource(nameof(SerialableTestData))]
+    public void ArgumentsThatAreSerializableWork(SerializableType testData)
+    {
+      Assert.That(testData.Data, Is.Not.EqualTo(0));
+    }
+
+    [Serializable]
+    public class SerializableType
+    {
+      public SerializableType(int data)
+      {
+        Data = data;
+      }
+
+      public int Data { get; }
+
+      /// <inheritdoc />
+      public override string ToString()
+        => Data.ToString();
+    }
+  }
+}
