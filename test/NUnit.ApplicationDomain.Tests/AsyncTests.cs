426ee992c016f304d60b33d0e511700ea9aaf027
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-10-15 00:04:17 -0500

    Add test to show problem of issue #18

---
+++ test/NUnit.ApplicationDomain.Tests/AsyncTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit.ApplicationDomain.Tests
+{
+  [RunInApplicationDomain]
+  internal class AsyncTests
+  {
+    public static bool DidGetSet;
+
+    [Test]
+    public async Task VerifyAsync()
+    {
+      await Task.Delay(TimeSpan.FromSeconds(1));
+
+      AppDomainRunner.DataStore.Set("", true);
+      DidGetSet = true;
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+      if (AppDomainRunner.IsInTestAppDomain)
+      {
+        Assert.That(DidGetSet, Is.True);
+      }
+      else
+      {
+        Assert.That(DidGetSet, Is.False);
+      }
+
+      Assert.That(AppDomainRunner.DataStore.Get<bool>(""), Is.True);
+    }
+  }
+}
